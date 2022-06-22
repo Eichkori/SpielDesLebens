5e51e8d5bb5806b0dc07b86b2fa37ce358132fd2
n-eichkorn <niklas.eichkorn@ametek.com>
2022-06-22 18:15:29 +0200

    writing class SpielDesLebens.py

---
+++ SpielDesLebend.py
@@ -0,0 +1,5 @@
+class SpielDesLebens:
+    def __init__(self):
+        self.field_size = 25
+        self.field = [][]
+        
--- Zelle.py
+++ Zelle.py
@@ -6,7 +6,7 @@ class Zelle(threading.Thread):
     def __init__(self, spiel, position):
         self.position = position
         self.spiel = spiel
-        self.isALive = True
+        self.is_Alive = True
 
     def getPosition(self):
         return self.position
@@ -25,5 +25,10 @@ class Zelle(threading.Thread):
             sleep(100)
         except InterruptedError:
             print("Fehler")
+        #while(self.is_Alive):
+            #self.spiel <- here call the Funktion in the SpielDesLebens Class
+        
+    def stop_Tread(self):
+        self.is_Alive = False
     #Siehe https://openbook.rheinwerk-verlag.de/python/32_005.html
     #Und https://docs.python.org/3/library/threading.html#condition-objects
